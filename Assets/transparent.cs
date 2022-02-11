e2e56a16242e92e2419fbce4198d5f3f994fbcf1
Ines Said <43462511+inessaid@users.noreply.github.com>
2022-02-11 15:24:49 -0500

    activate after few seconds

--- Assets/transparent.cs
+++ Assets/transparent.cs
@@ -9,17 +9,24 @@ public class transparent : MonoBehaviour
     // Start is called before the first frame update
     public Material mymaterial;
     public GameObject gb;
+    public GameObject gameObject;
     void Start()
     {
         // gb = MarsRuntimeUtils.GetActiveCamera(true).GetComponent<ARCameraManager>().gameObject;
-         
-        
+
+        Invoke("wakeup", 5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        mymaterial = MarsRuntimeUtils.GetActiveCamera(true).GetComponent<ARCameraManager>().cameraMaterial;
-        mymaterial.color = Color.clear;
+        //mymaterial = MarsRuntimeUtils.GetActiveCamera(true).GetComponent<ARCameraManager>().cameraMaterial;
+        //mymaterial.color = Color.clear;
+    }
+
+
+    void wakeup()
+    {
+        gameObject.active = true;
     }
 }
